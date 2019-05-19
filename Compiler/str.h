f55e3034e970086224ece19d2640daa252a3473f
Ehsan Alem <mail2ehsanalem@gmail.com>
2019-05-19 20:59:43 +0700

    add #include istream fstream iostream to avoid C2679 error on line 57 Compiler/str.h

--- Compiler/str.h
+++ Compiler/str.h
@@ -7,7 +7,11 @@ Now mapped onto the Standard Template Library string class (BSM, Sep 2002)
 */
 #pragma once
 
+#include <iostream>
+#include <fstream>
+#include <istream>
 #include <string>
+
 typedef std::basic_string<uint8_t, std::char_traits<uint8_t>, std::allocator<uint8_t>> Str;
 
 #include "..\VMPointers.h"
